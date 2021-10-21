614ea14eb9ac877c741c9b191f3dadfb51e76599
fbara <fbara1@yahoo.com>
2021-10-21 14:36:03 -0500

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Frank Bara on 10/21/21.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
